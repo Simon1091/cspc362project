41d46483ffe398e159b42cdfc19cef254feb8640
Simon1091 <simonlee839@gmail.com>
2018-07-26 10:37:27 -0700

    CreditCard class

---
+++ CreditCard.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+class CreditCard {
+private:
+	std::string cardNumber;
+	int cwid;
+	std::string expirationDate;
+	std::string cardHolderName;
+public:
+	void setcardNumber(std::string cardNumber) {
+		cardNumber = this->cardNumber;
+
+	}
+	void setcwid(int cwid) {
+		cwid = this->cwid;
+
+	}
+	void setexpirationDate(std::string expirationDate) {
+		expirationDate = this->expirationDate;
+
+	}
+	void setnameOfOwner(std::string cardHolderName) {
+		cardHolderName = this->cardHolderName;
+	}
+
+};
