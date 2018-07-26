f82309698810557ffae5fb3a3ec2f7851f300055
Simon1091 <simonlee839@gmail.com>
2018-07-26 11:00:19 -0700

    added switch case to Review Cart function
    Co-Authored-By: simon1091 <simon1091@users.noreply.github.com>

--- ReviewCartFolder/ReviewCart.h
+++ ReviewCartFolder/ReviewCart.h
@@ -2,6 +2,24 @@
 #include <iostream>
 #include <string>
 #include "Payment.h"
+
+void checkout() {
+	int orderChoice;
+	do {
+		std::cout << "Please fill out order\n1.Payment\n2.Shipping\n3.Discount\n4.Confirm";
+		switch (orderChoice) {
+		case 1://Payment
+			break;
+		case 2://Shipping
+			break;
+		case 3://Discount
+			break;
+		case 4://Confirm
+
+		}
+
+	} while (orderChoice != 4);
+}
 //Payment
 void printMessage() {
 	std::cout << "Please add credit/debit card Info";
