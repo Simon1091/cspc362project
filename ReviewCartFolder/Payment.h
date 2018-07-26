c7bef1e610710a69622dc880082272f2ee415442
Simon1091 <simonlee839@gmail.com>
2018-07-26 11:11:05 -0700

    checkout
    Co-Authored-By: simon1091 <simon1091@users.noreply.github.com>

--- ReviewCartFolder/Payment.h
+++ ReviewCartFolder/Payment.h
@@ -2,6 +2,16 @@
 #include <iostream>
 #include <string>
 #include "CreditCard.h"
+#include "ShoppingCart.h"
+class Payment {
+private:
+	ShoppingCart cart;
+	CreditCard card;
+	std::string billingAddress;
+	std::string country;
+	int zipcode;
+public:
 
+};
 
 
